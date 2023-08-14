2d9cb78f030533edb87e2e38afcc07d05f4e0abb
elangrg <45620769+elangrg@users.noreply.github.com>
2023-08-14 16:21:29 +0530

    all

---
+++ CodeFirstWithMigration/Migrations/20230814093958_dbInitAddCandidateTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirstWithMigration.Migrations
+{
+    public partial class dbInitAddCandidateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    CandidateID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CandidateName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.CandidateID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
---
+++ CodeFirstWithMigration/Models/Candidate.cs
@@ -0,0 +1,50 @@
+﻿using Humanizer;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstWithMigration.Models
+{
+    public class Candidate
+    {
+        public int CandidateID { get; set; }
+        public string CandidateName { get; set; }
+
+        public string Address
+        { get; set; }
+
+
+
+
+
+    }
+
+    public class CandidateContext : DbContext
+    {
+
+        public DbSet<Candidate> Candidates { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Candidate>().HasKey(c => c.CandidateID);
+            modelBuilder.Entity<Candidate>().Property(c => c.CandidateID).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Candidate>().Property(c => c.CandidateName).IsRequired().HasMaxLength(50).IsUnicode(false);
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=tmpIBMREactCFDb;Integrated Security=True;");
+
+        }
+
+
+
+    }
+    
+
+
+}
